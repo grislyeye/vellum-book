f0fc8ab7371913f98a1dd456eacd3549e56929f1
R.G. Wood <ric@grislyeye.com>
2026-02-08 18:01:38 +0000

    [wip] Delint

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ marked.use({
 
 const markdown = fs.readFileSync("README.md", "utf8");
 
-const html = marked
-  .use(markedFootnote())
-  .parse(markdown);
+const html = marked.use(markedFootnote()).parse(markdown);
 
 engine
   .renderFile("index.liquid", { content: html, project: package })
