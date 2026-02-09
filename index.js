3dff0d7d5c465ddf8ec278ea131e60c64c6fc73c
R.G. Wood <ric@grislyeye.com>
2026-02-09 23:15:38 +0000

    Add: sanitise HTML

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { marked } from "marked";
 import markedFootnote from "marked-magickcss-sidenote";
 import project from "./package.json" with { type: "json" };
 import { Liquid } from "liquidjs";
+import { JSDOM } from "jsdom";
+import DOMPurify from "dompurify";
 
+const window = new JSDOM("").window;
+const purify = DOMPurify(window);
 const engine = new Liquid();
 
 marked.use({
@@ -12,6 +16,8 @@ marked.use({
 
 const markdown = fs.readFileSync("README.md", "utf8");
 const html = marked.use(markedFootnote()).parse(markdown);
+
+engine.registerFilter("safe", (html) => purify.sanitize(html));
 
 engine
   .renderFile("index.liquid", { content: html, project: project })
